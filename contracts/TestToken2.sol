2e2f65ae36e70833a3871e5539a560a3b1d6be69
Gavin Francis <gav@eh7.co.uk>
2021-03-13 23:45:30 +0000

    feature: add Ownable contract, EIP-173, (contract TestToken2 as Ownable)

--- contracts/TestToken2.sol
+++ contracts/TestToken2.sol
@@ -1,5 +1,7 @@
 pragma solidity >=0.4.22 <0.7.0;
 
+import "./Ownable.sol";
+
 library BlacklistLib2 {
 
   function lengthOf(address[] storage self) view internal returns (uint256) {
@@ -31,7 +33,7 @@ library BlacklistLib2 {
 }
 
 // contract Test {
-contract TestToken2 {
+contract TestToken2 is Ownable {
 
   string  public name = "TestToken";
   string  public symbol = "Test";
@@ -43,7 +45,7 @@ contract TestToken2 {
   address[] public blacklist;
   using BlacklistLib2 for *;
 
-  address public owner;
+//  address public owner;
 //  uint256 public totalSupply;
   uint256 public startDate;
 
@@ -65,7 +67,7 @@ contract TestToken2 {
   mapping(address => mapping(address => uint256)) public allowed;
 
   constructor(uint256 _startDate, uint256 _initialSupply, uint256 _initialUSDForToken) public {
-    owner = msg.sender;
+ //   owner = msg.sender;
     totalSupply = 0;
     startDate = _startDate;
 
@@ -153,88 +155,3 @@ contract TestToken2 {
     return (totalSupply * priceUSDForToken);
   }
 }
-
-/*
-contract TestToken {
-  string  public name = "TestToken";
-  string  public symbol = "Test";
-  uint256 public totalSupply;
-  uint8   public decimals = 0;
-
-  uint256 priceUSDForToken;
-
-  address public owner;
-
-//  using Blacklist for address[];
-  address[] blacklist;
-  using Blacklist for *;
-
-  event Transfer(
-    address indexed _from,
-    address indexed _to,
-    uint256 _value
-  );
-
-  event Approval(
-    address indexed _owner,
-    address indexed _spender,
-    uint256 _value
-  );
-
-  mapping(address => uint256) public balanceOf;
-  mapping(address => mapping(address => uint256)) public allowed;
-
-  constructor(uint256 _initialSupply, uint256 _initialUSDForToken) public {
-    owner = msg.sender;
-    balanceOf[msg.sender] = _initialSupply;
-    totalSupply = _initialSupply;
-    priceUSDForToken = _initialUSDForToken;
-  }
-
-  function transfer(address _to, uint256 _value) public returns (bool success) {
-    require(balanceOf[msg.sender] >= _value);
-    balanceOf[msg.sender] -= _value;
-    balanceOf[_to] += _value;
-    emit Transfer(msg.sender, _to, _value);
-    return true;
-  }
-
-  function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
-    require(_value <= balanceOf[_from]);
-    require(_value <= allowed[_from][msg.sender]);
-    balanceOf[_from] -= _value;
-    balanceOf[_to] += _value;
-    allowed[_from][msg.sender] -= _value;
-    emit Transfer(_from, _to, _value);
-    return true;
-  }
-
-  function approve(address _spender, uint256 _value) public returns (bool success) {
-    allowed[msg.sender][_spender] = _value;
-    emit Approval(msg.sender, _spender, _value);
-    return true;
-  }
-
-  function allowance(address _owner, address _spender) public view returns (uint256 remaining) {
-    return allowed[_owner][_spender];
-  }
-
-  function getTokenUSDRate() public view returns (uint256 price) {
-    return priceUSDForToken;
-  }
-
-  function setTokenUSDRate(uint256 _rate) public returns (bool success) {
-    // QUESTIONS: should this function be only accessed
-    // by the contract owner?
-    // currently anyone can set this!
-    priceUSDForToken = _rate;
-    return true;
-  }
-
-  function getTotalUSDPriceMintedTokens() public view returns (uint256 price) {
-    // check if contract owner if not throw
-    require(owner == msg.sender);
-    return (totalSupply * priceUSDForToken);
-  }
-}
-*/
